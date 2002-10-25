cddf8f3f9630d7d8b8ab7f6348d639b7d32fb1dc
cerquide <cerquide>
2002-10-25 00:57:27 +0000

    Added

--- Durin/PP/Sampling/bin/BayesianAndChernoffBoundGraph.pl
+++ Durin/PP/Sampling/bin/BayesianAndChernoffBoundGraph.pl
@@ -16,13 +16,13 @@ if ($#ARGV == 0) {
 }
 
 my $N = 10000;
-my $n = 250;
+my $C = 0.9;
 
-plotReal($N,$n);
+plotReal($N,$C);
 
 sub plotReal
   {
-    my ($N,$n) = @_;
+    my ($N,$C) = @_;
     
     my $color = 1;
     
@@ -35,10 +35,10 @@ sub plotReal
     {
 	push @accuracies,(0.1 * $i/$items);
     }
-    env 0,0.1, 0,1, 0,-2;
-    pglab("Accuracy","Confidence",""); 
-    pgaxis("N", 0,0 , 0.1,0 , 0,0.1, 0.01 , 0,0.5,0,0,1,0);
-    pgaxis("N", 0,0 , 0,1 , 0,1, 0.1 , 0,0.5,0,0,-1,0);
+    env 0.02,0.1, 0,1000, 0,-2;
+    pglab("Accuracy","Sample size","Confidence = $C"); 
+    pgaxis("N", 0.02,0 , 0.1,0 , 0.02,0.1, 0.01 , 0,0.5,0,0,1,0);
+    pgaxis("N", 0.02,0 , 0.02,1000 , 0,1000, 100 , 0,0.5,0,0,-1,0);
     
     #pgtext(0.02,0.6,"Bayesian");
     #pgtext(0.042,0.6,"Chernoff");
@@ -52,8 +52,8 @@ sub plotReal
     foreach my $eps (@accuracies)
       {
 	set $x, $i, $eps;
-	set $ycher, $i, Durin::PP::Sampling::SamplingBounds::ChernoffAddConfidence($n,$eps);
-	set $ycer, $i, Durin::PP::Sampling::SamplingBounds::CerquidesAddConfidence($N,$n,int($n * 0.5),$eps);
+	set $ycher, $i, Durin::PP::Sampling::SamplingBounds::ChernoffBound($C,$eps);
+	set $ycer, $i, Durin::PP::Sampling::SamplingBounds::CerquidesBound($N,$C,$eps);
 	$i++;
       }	
     my @line_styles =();
@@ -61,5 +61,5 @@ sub plotReal
     line($x,$ycer,{LINESTYLE=>$color++});  hold();
     push @line_styles,$color;
     line($x,$ycher,{LINESTYLE=>$color++});  hold();
-    legend ["Bayesian","Chernoff"],0.08,0.15,{LineStyle =>\@line_styles};
+    legend ["Bayesian","Chernoff"],0.025,100,{LineStyle =>\@line_styles};
   }
---
+++ Durin/PP/Sampling/bin/BayesianChernoffDifferenceGraph.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+use strict;
+ 
+use IO::File;
+use Durin::Utilities::StringUtilities;
+use POSIX;
+use PDL;
+use PDL::Graphics::PGPLOT;
+use PGPLOT;
+use Durin::PP::Sampling::SamplingBounds;
+
+my $device = "/XSERVE";
+if ($#ARGV == 0) {
+  $device = $ARGV[0];
+}
+
+my $N = 10000;
+
+my $samplingSizes = [50,100,250,1000];
+my @accuracies = plotDifferenceInit();
+my @line_styles =();
+my $style = 1;
+foreach my $n (@$samplingSizes) {
+  plotDifference($N,$n,\@accuracies,$style);
+  push @line_styles,$style;
+  $style++;
+}
+legend $samplingSizes,0.08,0.17,{LineStyle =>\@line_styles};
+
+sub plotDifferenceInit {
+  
+  dev $device;
+  pgpap(5,1);
+  
+  my $items = 50;
+  my @accuracies = ();
+  for (my $i = 0; $i < $items ; $i++)
+    {
+      push @accuracies,(0.1 * $i/$items);
+    }
+  env 0,0.1, 0,1, 0,-2;
+  pglab("Accuracy","Confidence","Bayesian confidence - Chernoff confidence"); 
+  pgaxis("N", 0,0 , 0.1,0 , 0,0.1, 0.01 , 0,0.5,0,0,1,0);
+  pgaxis("N", 0,0 , 0,1 , 0,1, 0.1 , 0,0.5,0,0,-1,0);
+  
+  hold();
+  return @accuracies;
+}
+
+sub plotDifference {
+  my ($N,$n,$accuracies,$style) = @_;
+    
+  my @accuracies = @$accuracies;
+  
+  my $yreal = zeroes scalar(@accuracies);
+  my $ycer = zeroes scalar(@accuracies); 
+  my $ycher = zeroes scalar(@accuracies);	
+  my $x = zeroes scalar(@accuracies);
+  my $i = 0;
+  foreach my $eps (@accuracies)
+    {
+      set $x, $i, $eps;
+      set $ycher, $i, Durin::PP::Sampling::SamplingBounds::ChernoffAddConfidence($n,$eps);
+      set $ycer, $i, Durin::PP::Sampling::SamplingBounds::CerquidesAddConfidence($N,$n,int($n * 0.5),$eps);
+      $i++;
+    }	
+  my $diff = $ycer-$ycher;
+ 
+  line($x,$diff,{LINESTYLE=>$style});  hold();
+}
