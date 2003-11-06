38b451dd3b8f2684e89e29fbe59087a21c9de076
cerquide <cerquide>
2003-11-06 21:46:00 +0000

    *** empty log message ***

---
+++ Durin/Classification/Experimentation/ArtificialExperiment.pm
@@ -0,0 +1,154 @@
+#
+# This object is the base for artificial experiments from November 2003 on.
+
+package Durin::Classification::Experimentation::ArtificialExperiment;
+
+use base Durin::Classification::Experimentation::Experiment2;
+use Class::MethodMaker get_set => [-java => qw/ ModelGenerationCharacteristics LearningSampleSizes EvaluationCharacteristics/];
+
+
+
+use Durin::Classification::Experimentation::ResultTable;
+use Durin::ModelGeneration::ModelGenerator;
+use Durin::Classification::Experimentation::ModelTester;
+
+use strict;
+use warnings;
+
+use PDL;
+
+sub new_delta 
+  {
+    my ($class,$self) = @_;
+  }
+
+sub clone_delta
+  {  
+    # my ($class,$self,$source) = @_;
+      
+    die "Durin::Classification::Experimentation::Experiment2::clone not implemented\n";
+  }
+
+sub run  {   
+  my ($self) = @_;
+
+  
+  my $runs = $self->getRuns();
+  my $inducers = $self->getInducers();
+  my $resultDir = $self->getResultDir();
+  my $expName = $self->getName();
+  my $modelGenerationCharateristics = $self->getModelGenerationCharacteristics();
+  my $learningSampleSizes = $self->getLearningSampleSizes();
+  my $evaluationCharacteristics = $self->getEvaluationCharacteristics();
+  
+  # Create the model generator
+  
+  my $modelGenerator = Durin::ModelGeneration::ModelGenerator->create($modelGenerationCharateristics);
+  
+  # Construct the inducer list
+  
+  my $inducerList = $self->constructInducerList($inducers);
+  
+  # Create the tester
+  
+  my $tester = Durin::Classification::Experimentation::ModelTester->create($evaluationCharacteristics);
+  #$self->constructTester($evaluationCharacteristics);
+  
+  # Create the table where we store the results
+  
+  my $resultTable = Durin::Classification::Experimentation::ResultTable->new();
+  
+  for (my $run = 0; $run < $runs ; $run++) {
+    $self->executeRun($run,$modelGenerator,$inducerList,$learningSampleSizes,$tester,$resultTable);
+  }
+  
+  # do something with the result table.
+  $resultTable->loadValuesAndAverages();
+  $resultTable->writeSummary(">$resultDir"."$expName/summary.out");
+  #$resultTable->summarizeBayes();
+  #$resultTable->
+}
+  
+sub constructInducerList {
+  my ($self,$inducerNamesList) = @_;
+  
+  my $inducers = [];
+  foreach my $inducerName (@$inducerNamesList)
+    {
+      push @$inducers,Durin::Classification::Registry->getInducer($inducerName);
+    }
+  return $inducers;
+}
+
+
+
+sub executeRun {
+  my ($self,$runId,$modelGenerator,$inducerList,$learningSampleSizes,$tester,$resultTable) = @_;
+  
+  # Generate model
+  
+  $modelGenerator->run();
+  my $model = $modelGenerator->getOutput()->[0];
+  print "Model generated\n";
+  
+  $tester->setRealModel($model);
+  foreach my $size (@$learningSampleSizes) {
+    print "Starting dataset generation\n";
+    
+    my $trainingSet = $model->generateDataset($size);
+    
+    print "Dataset generated\n";
+    
+    print "Started learning\n";
+    
+    my $learntModels = $self->learnModels($inducerList,$trainingSet);
+
+    print "Models have been learnt\n";
+
+    print "Started testing\n";
+    
+    $self->testModels($runId,$size,$learntModels,$tester,$resultTable);
+    
+    print "Testing finished\n";
+    
+    print "Summarizing testing info\n";
+    
+    $tester->summarize($resultTable);
+  }
+}
+
+sub learnModels {
+  my ($self, $inducerList,$learningSet) = @_;
+  
+  my $bc = Durin::ProbClassification::ProbApprox::Counter->new();
+  $bc->setInput({TABLE => $learningSet,
+		 ORDER=> 2});
+  $bc->run();
+  my $countingTable = $bc->getOutput(); 
+  
+  my $models = [];
+  foreach my $inducer (@$inducerList) {
+    $inducer->setInput({TABLE => $learningSet, COUNTING_TABLE => $countingTable});
+    $inducer->run();
+    my $model = $inducer->getOutput();
+    #my $tree = $model->getTree();
+    #print $inducer->getName()." tree:\n";
+    #print $tree->makestring;
+    push @$models,$model
+  }
+  return $models;
+}
+
+sub testModels {
+  my ($self,$runId,$size,$models,$tester,$resultTable) = @_;
+  
+  foreach my $model (@$models) {
+    my $modelApplication = $tester->test($model);
+    $resultTable->addResult($runId,$size,$model,$modelApplication);
+  }
+}
+
+# End
+
+
+1;
---
+++ Durin/Classification/Experimentation/BayesModelTester.pm
@@ -0,0 +1,82 @@
+package Durin::Classification::Experimentation::BayesModelTester;
+
+@ISA = qw(Durin::Classification::Experimentation::ModelTester);
+
+use Class::MethodMaker get_set => [-java => qw/ Type Sample /];
+
+use strict;
+use warnings;
+
+use Durin::Classification::Experimentation::AUCModelApplier;
+
+use constant Bayes => "Bayes";
+use constant Sample => "Sample";
+
+sub new_delta
+  {
+    my ($class,$self) = @_;
+    
+ #   $self->{METADATA} = undef; 
+  }
+
+sub clone_delta
+  { 
+    my ($class,$self,$source) = @_;
+    
+    #   $self->setMetadata($source->getMetadata()->clone());
+  }
+
+sub init {
+  my ($self,$characteristics) = @_;
+  
+  my $name = $characteristics->{METHOD_NAME};
+  my $tester;
+  if (Bayes eq $name) {
+    $self->setType(Bayes);
+  } elsif (Sample eq $name) {
+    $self->setType(Sample);
+  }
+}
+
+sub test {
+  my ($self,$model) = @_;
+  
+  my $input = $self->getInput();
+  
+  
+  # Get the sample  over which the Bayes error rate is going to be approximated
+  #if (defined $input->{SAMPLE}) {
+  
+  if (Bayes eq $self->getType()) {
+    if (!defined $self->getSample()) { 
+      my $testingSet = $model->getSchema()->generateCompleteDatasetWithoutClass();
+      $self->setSample($testingSet);
+    }
+  }
+  if (!defined $self->getSample()) {
+    die "No sample passed as parameter\n";
+  }
+  my $sample = $self->getSample();
+  #my $expectedErrorRate = 0;
+  #my $count = 0;
+  my $applier = Durin::Classification::Experimentation::AUCModelApplier->new();
+  if (Bayes eq $self->getType()) {
+    $applier->setInput({TABLE => $sample, MODEL => $model, REAL_MODEL => $self->getRealModel(), BAYES => 1});
+  } elsif (Sample eq $self->getType()) {
+    $applier->setInput({TABLE => $sample, MODEL => $model});
+  }
+  $applier->run();
+  return $applier->getOutput();
+  #$resultTable->addResult($runId,$trainProportion,$model->getName(),$applier->getOutput());
+}
+
+sub summarize {
+  my ($self,$resultTable) = @_;
+  if (Bayes eq $self->getType()) {
+    $resultTable->summarizeBayes();
+  } else {
+    $resultTable->summarize();
+  }
+}
+
+1;
